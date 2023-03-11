d5f28fac7f48600c301818aa249220d41903332f
Bill Gribble <grib@billgribble.com>
2023-03-11 10:07:10 -0500

    Update metadata

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@
 # build with 'python ./setup.py install'
 from distutils.core import setup
 
-def shcall(cmdline):
-    from subprocess import Popen,PIPE
-    return Popen(cmdline.split(), stdout=PIPE).communicate()[0].decode()
-
-def git_version():
-    vers = shcall(b"git show --oneline").split('\n')[0].split(' ')[0]
-    if not isinstance(vers, str):
-        vers = vers.decode()
-    return str(int(vers.strip(), 16))
-
 VERSION = "0.0.1"
 
 setup(
@@ -23,7 +13,7 @@ setup(
     description = 'Async RPC toolkit',
     author = 'Bill Gribble',
     author_email = 'grib@billgribble.com',
-    uri = 'https://github.com/bgribble/carp',
+    url = 'https://github.com/bgribble/carp',
     download_url = 'https://github.com/bgribble/carp/archive/refs/tags/v0.0.1.zip',
     keywords = ['rpc', 'protobuf', 'json'],
     install_requires = [
